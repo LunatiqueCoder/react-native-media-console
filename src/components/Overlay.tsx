ee99ff778dc57f5ff93c5fb182d7af5b7362a44a
Elyse Segebart <elyse.segebart@musco.com>
2023-02-09 14:22:18 -0600

    [+] Make opacity darker on controls

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -22,7 +22,7 @@ const _styles = StyleSheet.create({
     right: 0,
     left: 0,
     bottom: 0,
-    backgroundColor: 'rgba(0,0,0,.3)',
+    backgroundColor: 'rgba(0,0,0,.6)',
     opacity: 0,
   },
 });
