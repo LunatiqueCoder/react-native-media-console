692271e883106ccf66fb4cd80f1061274f938454
Léa De Amorin <43986934+leadmrn@users.noreply.github.com>
2023-02-23 17:07:33 +0100

    fix forward button

--- src/VideoPlayer.tsx
+++ src/VideoPlayer.tsx
@@ -427,8 +427,14 @@ export const VideoPlayer = (props: VideoPlayerProps) => {
               onPressRewind={() =>
                 videoRef?.current?.seek(currentTime - rewindTime)
               }
-              onPressForward={() =>
-                videoRef?.current?.seek(currentTime + rewindTime)
+              onPressForward={
+                () => {
+                  if (currentTime + rewindTime > duration) {
+                    videoRef?.current?.seek(duration - 0.1);
+                  } else {
+                    videoRef?.current?.seek(currentTime + rewindTime);
+                  }
+                }
               }
             />
             <BottomControls
